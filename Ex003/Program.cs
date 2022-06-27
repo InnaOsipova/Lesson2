836963c411e09fdada675c79c876577ae0593aec
Inna Osipova <rutheva_inna@mail.ru>
2022-06-27 21:15:49 +0300

    из трехзначного числа сделали двухзначное

---
+++ Ex003/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int Number ()
+{
+    int num = new Random().Next(100,1000);
+    int first = num/100;
+    int third = num%10;
+    int num1 = first*10 + third;
+    Console.WriteLine(num+"->");
+
+    return(num1);
+}
+
+Console.WriteLine(Number());
