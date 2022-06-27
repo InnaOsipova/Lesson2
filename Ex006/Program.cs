5ede45270a68248b90ebbcf036a23310b59109c1
Inna Osipova <rutheva_inna@mail.ru>
2022-06-27 22:20:02 +0300

    число квадрат другого

---
+++ Ex006/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.WriteLine("Введите число1");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число2");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+void Function (double str1, double str2)
+{
+    if ( str1 == str2*str2)
+    {
+        Console.WriteLine(" первое число является кватратом второго");
+    }
+    else if (str2 == str1*str1)
+    {
+        Console.WriteLine("второе число является кватратом перового ");
+    }
+    else
+    {
+        Console.WriteLine("квадратов нет ");
+    }
+}
+
+Function(num1, num2);
