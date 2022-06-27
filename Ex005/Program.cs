c664d5bb14bae3cfd6429c5d8f4df6f16186cf6e
Inna Osipova <rutheva_inna@mail.ru>
2022-06-27 22:09:18 +0300

    кратное

---
+++ Ex005/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход число 
+//и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Введите число");
+double num = Convert.ToDouble(Console.ReadLine());
+
+void Function (double str)
+{
+    if ((str % 7 == 0) && ( str % 23 == 0))
+    {
+        Console.WriteLine("Является кратным");
+    }
+    else
+    {
+        Console.WriteLine(" НЕ является кратным");
+    }
+}
+
+Function(num);
