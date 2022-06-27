91a63320f078fdcf62f031eada80c86c97935cbb
Inna Osipova <rutheva_inna@mail.ru>
2022-06-27 20:46:16 +0300

    максимальное число из случайного числа

---
+++ Ex002/Program.cs
@@ -0,0 +1,18 @@
+﻿int showNumber()
+{
+  int num = new Random().Next(10,99);
+  int first = num/10;
+  int second = num%10;
+
+  Console.Write(num+"->");
+
+  if (first>second)
+  {
+    return(first);
+  }
+  else
+  {
+    return(second);
+  }
+};
+Console.WriteLine(showNumber());
