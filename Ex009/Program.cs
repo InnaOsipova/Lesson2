4ef819351c1855827dbf42f639c996892437143c
Inna Osipova <rutheva_inna@mail.ru>
2022-06-28 08:55:27 +0300

    массив

---
+++ Ex009/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {23, 56, 46, 45, 44, 35, 44, 56, 47, 30};
+int n = array.Length;
+
+Console.Write("Введите искомое число :");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+
+while(index < n)
+{
+    if (array[index] == num)
+    {
+        Console.Write("Индекс нужного элемента :"+ index);
+        break;
+        //index++;
+    }
+    index++;
+}
